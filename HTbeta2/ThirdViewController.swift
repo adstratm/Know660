9caa478a41c2f616c68679979def0c06a4c7a88b
Andrew Stratmann <ads5743@truman.edu>
2016-03-19 13:16:26 -0500

    First Version

---
+++ HTbeta2/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  HTbeta2
+//
+//  Created by Andrew Stratmann on 3/19/16.
+//  Copyright © 2016 SinghStratmannWarren. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
