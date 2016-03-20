1565c4dcc4a6c1bc81314d26b0e115842f2c098e
Andrew Stratmann <ads5743@truman.edu>
2016-03-20 15:26:21 -0500

    Final Hackathon Version

--- HTbeta2/FirstViewController.swift
+++ HTbeta2/FirstViewController.swift
@@ -7,12 +7,101 @@
 //
 
 import UIKit
+import WebKit
+
 
 class FirstViewController: UIViewController {
 
+    
+    @IBAction func exampleButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://kirksvilledailyexpress.com")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+
+    
+    @IBAction func secondButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://www.missourigasprices.com/Kirksville/index.aspx")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func thirdButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://kirksvillecity.com")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func fourthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://kirksvillecity.com/content/76/default.aspx")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func fifthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://visitkirksville.com/events/")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func sixthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://visitkirksville.com")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func seventhButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://kirksville.k12.mo.us")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func eighthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://facebook.com/KirksvillePoliceDepartment")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func ninthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://kirksvilleatc.weebly.com")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func tenthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://truman.edu")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func eleventhButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://atsu.edu")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func fourteenthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://downtowncinema8.com")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func twelthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://thousandhillskirksville.com")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    @IBAction func thirteenthButton(sender: AnyObject)
+    {
+        let websiteAddress = NSURL(string: "http://capeair.com")
+        UIApplication.sharedApplication().openURL(websiteAddress!)
+    }
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
